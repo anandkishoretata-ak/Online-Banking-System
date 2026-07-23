9f1616eb0c8ec6284b5bb1f5d8974c6197de6bc2
Tata Anand kishore <anandkishoretata@gmail.com>
2026-07-23 14:57:18 +0530

    my modify code

--- src/pages/WithdrawMoney.jsx
+++ src/pages/WithdrawMoney.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Sidebar from "../components/Sidebar";
+import ProfileHeader from "../components/ProfileHeader";
 import "../styles/Banking.css";
 
 function WithdrawMoney() {
@@ -8,54 +10,53 @@ function WithdrawMoney() {
     e.preventDefault();
 
     if (!amount || Number(amount) <= 0) {
-      alert("Please enter a valid amount.");
+      alert("Please enter a valid amount");
       return;
     }
 
-    const newTransaction = {
-      type: "Withdraw",
-      amount,
-      date: new Date().toLocaleDateString(),
-      time: new Date().toLocaleTimeString(),
-    };
-
-    const transactions =
-      JSON.parse(
-        localStorage.getItem("transactions")
-      ) || [];
-
-    transactions.push(newTransaction);
-
-    localStorage.setItem(
-      "transactions",
-      JSON.stringify(transactions)
-    );
-
     alert(`₹${amount} withdrawn successfully!`);
 
-    setAmount("");
+    window.location.href = "/dashboard";
   };
 
   return (
-    <div className="banking-container">
-      <div className="banking-card">
-        <h2>Withdraw Money</h2>
+    <div className="dashboard">
 
-        <form onSubmit={handleWithdraw}>
-          <input
-            type="number"
-            placeholder="Enter Amount"
-            value={amount}
-            onChange={(e) =>
-              setAmount(e.target.value)
-            }
-          />
+      <Sidebar />
 
-          <button type="submit">
-            Withdraw
-          </button>
-        </form>
+      <div className="main-content">
+
+        <ProfileHeader />
+
+        <div className="banking-container">
+
+          <div className="banking-card">
+
+            <h2>🏧 Withdraw Money</h2>
+
+            <form onSubmit={handleWithdraw}>
+
+              <input
+                type="number"
+                placeholder="Enter Amount"
+                value={amount}
+                onChange={(e) =>
+                  setAmount(e.target.value)
+                }
+              />
+
+              <button type="submit">
+                Withdraw
+              </button>
+
+            </form>
+
+          </div>
+
+        </div>
+
       </div>
+
     </div>
   );
 }
