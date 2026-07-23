ea0c80b0a921b91a6d0fd98056cbcf045cc9ca7a
Tata Anand kishore <anandkishoretata@gmail.com>
2026-07-23 14:30:52 +0530

    my modify code

--- src/components/RecentTransactions.jsx
+++ src/components/RecentTransactions.jsx
@@ -1,31 +1,50 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function RecentTransactions() {
-  const transactions = [
-    {
-      id: 1,
-      type: "Deposit",
-      amount: 5000,
-    },
-    {
-      id: 2,
-      type: "Withdraw",
-      amount: 2000,
-    },
-    {
-      id: 3,
-      type: "Transfer",
-      amount: 1500,
-    },
-  ];
+  const [transactions, setTransactions] =
+    useState([]);
+
+  useEffect(() => {
+    const fetchTransactions =
+      async () => {
+        try {
+          const token =
+            localStorage.getItem("token");
+
+          const res = await axios.get(
+            "http://localhost:8000/api/transactions/history",
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+
+          setTransactions(
+            res.data.slice(0, 5)
+          );
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+    fetchTransactions();
+  }, []);
 
   return (
     <div className="card">
       <h3>Recent Transactions</h3>
 
-      {transactions.map((item) => (
-        <p key={item.id}>
-          {item.type} - ₹{item.amount}
-        </p>
-      ))}
+      {transactions.length === 0 ? (
+        <p>No Transactions Found</p>
+      ) : (
+        transactions.map((item) => (
+          <p key={item._id}>
+            {item.type} - ₹{item.amount}
+          </p>
+        ))
+      )}
     </div>
   );
 }
