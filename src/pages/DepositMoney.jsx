ca1384d51954badf7599507bec2f801b4f1635f7
Tata Anand kishore <anandkishoretata@gmail.com>
2026-07-23 14:37:13 +0530

    my modify code

--- src/pages/DepositMoney.jsx
+++ src/pages/DepositMoney.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Sidebar from "../components/Sidebar";
+import ProfileHeader from "../components/ProfileHeader";
 import "../styles/Banking.css";
 
 function DepositMoney() {
@@ -8,54 +10,53 @@ function DepositMoney() {
     e.preventDefault();
 
     if (!amount || Number(amount) <= 0) {
-      alert("Please enter a valid amount.");
+      alert("Please enter a valid amount");
       return;
     }
 
-    const newTransaction = {
-      type: "Deposit",
-      amount,
-      date: new Date().toLocaleDateString(),
-      time: new Date().toLocaleTimeString(),
-    };
+   alert(`₹${amount} deposited successfully!`);
 
-    const transactions =
-      JSON.parse(
-        localStorage.getItem("transactions")
-      ) || [];
-
-    transactions.push(newTransaction);
-
-    localStorage.setItem(
-      "transactions",
-      JSON.stringify(transactions)
-    );
-
-    alert(`₹${amount} deposited successfully!`);
-
-    setAmount("");
+    window.location.href = "/dashboard";
   };
 
   return (
-    <div className="banking-container">
-      <div className="banking-card">
-        <h2>Deposit Money</h2>
+    <div className="dashboard">
 
-        <form onSubmit={handleDeposit}>
-          <input
-            type="number"
-            placeholder="Enter Amount"
-            value={amount}
-            onChange={(e) =>
-              setAmount(e.target.value)
-            }
-          />
+      <Sidebar />
 
-          <button type="submit">
-            Deposit
-          </button>
-        </form>
+      <div className="main-content">
+
+        <ProfileHeader />
+
+        <div className="banking-container">
+
+          <div className="banking-card">
+
+            <h2>💰 Deposit Money</h2>
+
+            <form onSubmit={handleDeposit}>
+
+              <input
+                type="number"
+                placeholder="Enter Amount"
+                value={amount}
+                onChange={(e) =>
+                  setAmount(e.target.value)
+                }
+              />
+
+              <button type="submit">
+                Deposit
+              </button>
+
+            </form>
+
+          </div>
+
+        </div>
+
       </div>
+
     </div>
   );
 }
