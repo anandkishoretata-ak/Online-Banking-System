8f1d6cc84a8b6b26f5d5da451d41c23fba960062
Tata Anand kishore <anandkishoretata@gmail.com>
2026-07-23 14:31:44 +0530

    my modify code

--- src/components/TransactionCard.jsx
+++ src/components/TransactionCard.jsx
@@ -1,9 +1,44 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function TransactionCard() {
+  const [count, setCount] =
+    useState(0);
+
+  useEffect(() => {
+    const fetchTransactions =
+      async () => {
+        try {
+          const token =
+            localStorage.getItem(
+              "token"
+            );
+
+          const res =
+            await axios.get(
+              "http://localhost:8000/api/transactions/history",
+              {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              }
+            );
+
+          setCount(res.data.length);
+
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+    fetchTransactions();
+  }, []);
+
   return (
     <div className="card">
       <h3>Total Transactions</h3>
 
-      <h1>150</h1>
+      <h1>{count}</h1>
     </div>
   );
 }
